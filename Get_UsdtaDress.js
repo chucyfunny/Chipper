36b1c50e57ff4b6a08c4d2422dc07e3d1a344d4a
chucyfunny <125993050+chucyfunny@users.noreply.github.com>
2024-08-17 11:21:56 +0800

    Update Get_UsdtaDress.js

--- Get_UsdtaDress.js
+++ Get_UsdtaDress.js
@@ -2,35 +2,10 @@ var headers = $request.headers;
 var authorization = headers['Authorization'];
 
 if (authorization) {
-    console.log(authorization);
-    // 定义TRON链的URL
-    var tronURL = "https://api.chippercash.com/v1/crypto/deposit/address?asset=USDT&chain=TRON";
-
-    // Function to handle the request and log the result
-    function fetchTronAddress() {
-        var requestHeaders = {
-            'Authorization': authorization,
-            'Content-Type': 'application/json'
-        };
-
-        // 发起GET请求以获取TRON链的存款地址
-        $task.fetch({ url: tronURL, headers: requestHeaders }).then(response => {
-            if (response.statusCode === 200) {
-                var address = JSON.parse(response.body).address;
-                console.log("TRON USDT Deposit Address: " + address);
-            } else {
-                console.log("Failed to retrieve TRON address. Status Code: " + response.statusCode);
-            }
-        }, reason => {
-            console.log("Request failed. Reason: " + reason.error);
-        });
-    }
-
-    // 调用函数获取TRON链的存款地址
-    fetchTronAddress();
-
+    // 使用圈X的通知功能显示令牌
+    $notify("Authorization Token", "Here is your token:", authorization);
 } else {
-    console.log("Authorization Token Not Found: The request did not contain an Authorization header.");
+    $notify("Authorization Token Not Found", "Error:", "The request did not contain an Authorization header.");
 }
 
 // 让请求正常通过
