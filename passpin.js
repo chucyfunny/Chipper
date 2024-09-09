38cf4bec8ad916853190f82af1c3b2f5d4975798
chucyfunny <125993050+chucyfunny@users.noreply.github.com>
2024-09-09 23:43:31 +0800

    Create passpin.js

---
+++ passpin.js
@@ -0,0 +1,19 @@
+// 构建伪造的响应
+const fakeResponse = {
+    success: true
+};
+
+// 设置 HTTP 头信息
+const headers = {
+    "Content-Type": "application/json",
+    "Connection": "keep-alive",
+    "Strict-Transport-Security": "max-age=2592000; includeSubDomains",
+    "Content-Length": JSON.stringify(fakeResponse).length.toString()
+};
+
+// 使用 Quantumult X 提供的 API 直接返回伪造的响应
+$done({
+    status: 200,
+    headers: headers,
+    body: JSON.stringify(fakeResponse)
+});
